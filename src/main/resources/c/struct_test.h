8d1b009178d5f718a451b295325cc581621aebaa
GitJavaProgramming <455547940@qq.com>
2019-12-13 14:18:57 +0800

    C语言知识 数据类型 预处理 结构体、指针与内存分配

---
+++ src/main/resources/c/struct_test.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_TEST_H_INCLUDED
+#define STRUCT_TEST_H_INCLUDED
+
+
+
+#endif // STRUCT_TEST_H_INCLUDED
+
+typedef int DATA_TYPE;
+typedef struct array {
+    DATA_TYPE * data;
+    size_t length;
+}Array;
+
+Array create_array(size_t size);
+
+_Bool add(DATA_TYPE data);
+
